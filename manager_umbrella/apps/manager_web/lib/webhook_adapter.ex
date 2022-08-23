9e01d74f2935491c87bb5a22f5a6db4df697c6c5
3n3a <46775561+3n3a@users.noreply.github.com>
2022-08-23 10:17:17 +0200

    Update webhook_adapter.ex

--- manager_umbrella/apps/manager_web/lib/webhook_adapter.ex
+++ manager_umbrella/apps/manager_web/lib/webhook_adapter.ex
@@ -1,3 +1,19 @@
 defmodule WebhookAdapter do
-  
+  @doc "
+    Adapt to various Webhooks
+  "
+
+  def trigger_deploy(url, host, api_key) do
+    @doc "
+      Trigger Frontend Deploy Webhooks
+    "
+
+    body = [host: host]
+    {_status, result} = JSON.encode(body)
+
+    options = [{"X-API-KEY", api_key}, {"Content-Type", "application/json"}]
+
+    HTTPoison.start
+    HTTPoison.post!(url, result, )
+  end
 end
