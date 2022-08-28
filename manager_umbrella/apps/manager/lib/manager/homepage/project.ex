96ee99226897ef46a01c3faaf78b85ebf3545b50
3n3a <info@3n3a.ch>
2022-08-28 14:34:14 +0200

    fix constraint error when createing new project

--- manager_umbrella/apps/manager/lib/manager/homepage/project.ex
+++ manager_umbrella/apps/manager/lib/manager/homepage/project.ex
@@ -18,6 +18,7 @@ defmodule Manager.Homepage.Project do
     project
     |> cast(attrs, [:title, :body, :href, :color, :customBg, :status])
     |> validate_required([:title, :body, :href, :status])
+    |> unique_constraint(:name)
     |> unique_constraint(:href)
     |> unique_constraint(:title)
   end
